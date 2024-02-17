8edbadfb0684c305e70e0f5bfe21c6297a9529a2
Olamide Alokan <alokanolamide@gmail.com>
2024-02-17 22:14:05 +0100

    [ADD] 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,16 +3,16 @@ const request = require('request');
 
 const getCharacterName = (url) => {
   request(url, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.statusCode === 200) {
       console.log(JSON.parse(body).name);
     }
   });
-}
+};
 
-function getCharactersOfMovie(movieId) {
+function getCharactersOfMovie (movieId) {
   const url = `https://swapi.dev/api/films/${movieId}`;
-  request(url, function(error, response, body){
-    if (!error && response.statusCode == 200) {
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
       const characters = JSON.parse(body).characters;
       for (let i = 0; i < characters.length; i++) {
         getCharacterName(characters[i]);
